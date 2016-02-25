334f37ccd2f60621602150fbaeb989f477224730
Pedro Luís <pedroalexandrecl@gmail.com>
2016-02-25 08:34:06 +0000

    Atualizado UserDAO

--- src/persistence/UserDAO.java
+++ src/persistence/UserDAO.java
@@ -14,7 +14,7 @@ import factories.UserFactory;
 
 public class UserDAO {
 	
-	private HashMap<String, User> users;
+	private HashMap<String, String> users;
 	
 	private UserFactory userFactory;
 	
@@ -25,17 +25,33 @@ public class UserDAO {
 	
 	
 	public User getUser(String username) {
-		return users.get(username);
+		String password = users.get(username);
+		User user = new User();
+		
+		if (password != null) {
+			user.setUsername(username);
+			user.setPassword(password);
+		}
+		
+		return user;
 	}
 	
 	
-	public void addUser(String username, String password) {
-		User user = userFactory.build(username, password);
-		users.put(username, user);
+	public boolean addUser(String username, String password) {
+		//User user = userFactory.build(username, password);
+		
+		User user = getUser(username);
+		
+		if (user == null) {
+			users.put(username, password);
+			return true;
+		}
+		
+		return false;
 	}
 	
 	
-	private HashMap<String, User>loadUsers() {
+	private HashMap<String, String>loadUsers() {
 		users = new HashMap<>();
 		
 		FileInputStream in;
@@ -63,26 +79,26 @@ public class UserDAO {
 			FileReader fr = new FileReader(file);
 			
 			br = new BufferedReader(fr);
-			UserFactory userFactory = new UserFactory();
+			//UserFactory userFactory = new UserFactory();
 			
 			while ((line = br.readLine()) != null) {
 				String[] args = line.split(" ");
 				String username = args[0];
 				String password = args[1];
 				
-				User user = userFactory.build(username, password);
+				//User user = userFactory.build(username, password);
 				
-				users.put(username, user);
+				users.put(username, password);
 				System.out.println(line);
 				
 				//criar pasta para o user
-				file = new File("users/" + user.getId());
+				file = new File("users/" + username);
 
 				if (!(file.exists() && file.isDirectory())) {
 					file.mkdir();
 				}
 				
-				file = new File("users/" + user.getId() + "/conversations");
+				file = new File("users/" + username + "/conversations");
 				
 				if (!file.exists()) {
 					file.createNewFile();
