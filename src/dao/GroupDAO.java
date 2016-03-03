35c51fa94ad1cb80ede6bc56865cda8f8ec89087
Pedro Luís <pedroalexandrecl@gmail.com>
2016-03-03 13:58:44 +0000

    Adicionados utils para ler e gravar objectos

--- src/dao/GroupDAO.java
+++ src/dao/GroupDAO.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 
 import entities.Group;
 import interfaces.dao.GroupDAOInterface;
+import util.MiscUtil;
 
 public class GroupDAO implements GroupDAOInterface {
 
@@ -31,27 +32,13 @@ public class GroupDAO implements GroupDAOInterface {
 	 */
 	@Override
 	public Group getGroupByName(String groupName) {
-		File file = new File("groups/" + groupName + "/group");
-		Group group = null;
-
-		FileInputStream fileInputStream;
-		try {
-			fileInputStream = new FileInputStream(file);
-			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-			group = (Group) objectInputStream.readObject();
-
-			objectInputStream.close();
-			fileInputStream.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		HashMap<String, Group> groups = (HashMap<String, Group>) MiscUtil.readObject("groups/groups");
+		
+		if (groups != null) {
+			return groups.get(groupName);
 		}
 
-		return group;
+		return null;
 	}
 
 	/**
@@ -64,59 +51,22 @@ public class GroupDAO implements GroupDAOInterface {
 	public void addUserToGroup(String groupName, String username) {
 		HashMap<String, Group> groups = getGroups();
 		
-		File file = new File("groups/groups");
-
-		// ficheiro nao existe
-		if (!file.exists()) {
-			file.getParentFile().mkdirs();
-
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		MiscUtil.createFile("groups/groups");
+		MiscUtil.createFile("groups/" + groupName + "/group");
 		
-		file = new File("groups/" + groupName + "/group");
-
-		// ficheiro nao existe
-		if (!file.exists()) {
-			file.getParentFile().mkdirs();
-
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
 		Group group = null;
 
+		group = getGroupByName(groupName);
+
+		// utilizador já adicionado ao grupo
+		if (group.getUsers().contains(username))
+			return;
+
+		// adicionar utilizador ao grupo
+		group.addUser(username);
+
 		// atualizar ficheiro
-		try {
-			group = getGroupByName(groupName);
-
-			// utilizador já adicionado ao grupo
-			if (group.getUsers().contains(username))
-				return;
-
-			// adicionar utilizador ao grupo
-			group.addUser(username);
-
-			// atualizar ficheiro
-			FileOutputStream fileOutputStream = new FileOutputStream(file);
-			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-			objectOutputStream.writeObject(group);
-
-			objectOutputStream.close();
-			fileOutputStream.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		MiscUtil.writeObject(group, "groups/groups");
 	}
 
 	@Override
--- src/util/MiscUtil.java
+++ src/util/MiscUtil.java
@@ -1,7 +1,14 @@
 package util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import entities.Group;
 
 public class MiscUtil {
 
@@ -20,5 +27,61 @@ public class MiscUtil {
 			System.out.println("Delete file " + f.getAbsolutePath());
 			f.delete();
 		}
+	}
+
+	public static void createFile(String filePath) {
+		File file = new File(filePath);
+
+		// ficheiro nao existe
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void writeObject(Object obj, String filePath) {
+		File file = new File(filePath);
+
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+			objectOutputStream.close();
+			fileOutputStream.close();
+			objectOutputStream.writeObject(obj);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object readObject(String filePath) {
+		File file = new File(filePath);
+		Object obj = null;
+		
+		try {
+			FileInputStream fileInputStream = new FileInputStream(file);
+			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+			obj = objectInputStream.readObject();
+
+			objectInputStream.close();
+			fileInputStream.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}	
+		
+		return obj;
 	}
 }
